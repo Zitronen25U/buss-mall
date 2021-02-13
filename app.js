db73b135779e832c150226b12dc3fc1b130dbf73
Brian Lemons <brian_lemons@hotmail.com>
2021-02-13 15:24:32 -0800

    finishing lab. results wont render

--- app.js
+++ app.js
@@ -1,35 +1,103 @@
 'use strict';
 
 let totalClicks = 0;
-let clicksAllowed = 15;
-let allBusses = [];
+let clicksAllowed = 25;
+let allAds = [];
 let imageOne = document.querySelector('section img:first-child');
 let imageTwo = document.querySelector('section img:nth-child(2)');
 let imageThree = document.querySelector('section img:nth-child(3)');
 let myContainer = document.querySelector('section'); //event listner
+let myButton = document.querySelector('div');
 
 
-function // contructor
-
-function getRandomIndex(){
-  return Math.floor(Math.random() * allBusses.length);
+function Newad(name, fileExtension = 'jpg'){
+  this.name = name;
+  this.src = `img/${name}.${fileExtension}`;
+  this.views = 0
+  this.click = 0;
+  allAds.push(this);
 }
 
-function render// (){
-  let firstBus = getRandomIndex();
-  let secondBus = getRandomIndex();
-  let thirdBus = getRandomIndex();
+new Newad('bag');
+new Newad('banana');
+new Newad('bathroom');
+new Newad('boots');
+new Newad('breakfast');
+new Newad('bubblegum');
+new Newad('chair');
+new Newad('cthulu');
+new Newad('dog-duck');
+new Newad('dragon');
+new Newad('pen');
+new Newad('pet-sweep');
+new Newad('scissors');
+new Newad('shark');
+new Newad('sweep', 'pn');
+new Newad('tauntaun');
+new Newad('unicorn');
+new Newad('usb', 'gif');
+new Newad('water-can');
 
+function getRandomIndex(){
+  return Math.floor(Math.random() * allAds.length);
+}
+
+
+function renderAds(){
+  let firstAdIndex = getRandomIndex();
+  let secondAdIndex = getRandomIndex();
+  let thirdAdIndex = getRandomIndex();
+  while(firstAdIndex === secondAdIndex || secondAdIndex === thirdAdIndex || firstAdIndex === thirdAdIndex){
+    secondAdIndex = getRandomIndex();
+    thirdAdIndex = getRandomIndex();
+  }
+
+  imageOne.src = allAds[firstAdIndex].src;
+  imageOne.title = allAds[firstAdIndex].name;
+  allAds[firstAdIndex].views++;
+
+  imageTwo.src = allAds[secondAdIndex].src;
+  imageTwo.title = allAds[secondAdIndex].name;
+  allAds[secondAdIndex].views++;
+
+  imageThree.src = allAds[thirdAdIndex].src;
+  imageThree.title = allAds[thirdAdIndex].name;
+  allAds[thirdAdIndex].views++
+}
+function renderResults(){
+  let adList = document.querySelector('ul');
+  for (let i = 0; i < allAds.length; i++){
+    let li = document.createElement('li');
+    li.textContent =  `${allAds[i].name} had ${allAds[i].view} votes, and was seen ${allAds[i].clicks} times`;
+    adList.appendChild(li);
+  }
 }
 
 function handleClick(event){
+  if (event.target === myContainer){
+    alert('Please click an image to take the survey');
+  }
   totalClicks++;
-  letBusClicked = event.target.title;
-  render//
-  if(totalClicks === clicksAllowed0{
-    //remove event listner
+  let adsClicked = event.target.title;
+
+  for (let i = 0; i < allAds.length; i++){
+    if(adsClicked === allAds[i].name){
+      allAds[i].clicks++;
+    }
+  }
+
+  renderAds();
+  if (totalClicks === clicksAllowed){
     myContainer.removeEventListener('click', handleClick);
   }
 }
 
+function handleButtonClick(event){
+  if(totalClicks === clicksAllowed){
+    renderResults();
+  }
+}
+
+renderAds();
 myContainer.addEventListener('click', handleClick);
+myButton.addEventListener('click', handleButtonClick);

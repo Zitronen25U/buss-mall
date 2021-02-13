90fb765659086decb2780b82618e5a3e7643e417
Brian Lemons <brian_lemons@hotmail.com>
2021-02-13 11:30:25 -0800

    setting up

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let totalClicks = 0;
+let clicksAllowed = 15;
+let allbusses = [];
+let imageOne = document.querySelector('section img:first-child');
+let imageTwo = document.querySelector('section img:nth-child(2)');
+let imageThree = document.querySelector('section img:nth-child(3)');
+
+imageOne();
+imageTwo();
+imageThree();

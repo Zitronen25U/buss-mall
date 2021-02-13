39a37488b5b15a3000d9f660397f0074ed25633c
Brian Lemons <brian_lemons@hotmail.com>
2021-02-13 12:33:04 -0800

    setting up basic lab stuffs

--- app.js
+++ app.js
@@ -2,11 +2,34 @@
 
 let totalClicks = 0;
 let clicksAllowed = 15;
-let allbusses = [];
+let allBusses = [];
 let imageOne = document.querySelector('section img:first-child');
 let imageTwo = document.querySelector('section img:nth-child(2)');
 let imageThree = document.querySelector('section img:nth-child(3)');
+let myContainer = document.querySelector('section'); //event listner
 
-imageOne();
-imageTwo();
-imageThree();
+
+function // contructor
+
+function getRandomIndex(){
+  return Math.floor(Math.random() * allBusses.length);
+}
+
+function render// (){
+  let firstBus = getRandomIndex();
+  let secondBus = getRandomIndex();
+  let thirdBus = getRandomIndex();
+
+}
+
+function handleClick(event){
+  totalClicks++;
+  letBusClicked = event.target.title;
+  render//
+  if(totalClicks === clicksAllowed0{
+    //remove event listner
+    myContainer.removeEventListener('click', handleClick);
+  }
+}
+
+myContainer.addEventListener('click', handleClick);
